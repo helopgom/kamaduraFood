2f152cad4ac2c2044c6dcbb59cc008ca620791ba
gabriela <gabrielalacueva@gmail.com>
2024-06-05 12:06:45 +0200

    nulo

--- src/menu.js
+++ src/menu.js
@@ -1,3 +1,18 @@
+import { products, filters } from './data.js';
+
 //DEBE imprimir en pantalla la información de filtros.
+document.addEventListener("DOMContentLoaded", () => {
+    const filtersContainer = document.getElementById('filters');
+    const productContainer = document.getElementById('products');
+  
+
+filters.forEach(filter => {
+    const filterButton = document.createElement('button');
+    filterButton.className = 'filter';
+    filterButton.textContent = filter;
+    filterButton.addEventListener('click', () => filterProducts(filter));
+    filtersContainer.appendChild(filterButton);
+});});
+
 
 //DEBE imprimir en pantalla los productos, con su Título, descripción y precio en € y botón de añadir.
