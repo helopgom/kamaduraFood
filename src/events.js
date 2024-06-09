faa641c193dc130d4fa66b0e1569b9b4e010139d
gabriela <gabrielalacueva@gmail.com>
2024-06-09 09:31:45 +0200

    modificado el Pop-Up

--- src/events.js
+++ src/events.js
@@ -1,12 +1,12 @@
 //Intenta separar los eventos en este archivo.
 
-
 document.getElementById("pay-button").addEventListener("click", function() {
-    Swal.fire({
-        title: 'Gracias por tu Compra',
-        text: '¡Pedido realizado con éxito, gracias por comprar en Kamakura Food!',
-        confirmButtonText: '<img src="assets/img/close.svg" style="background-color:transparent;">',
-        html:'<img src="assets/img/logo.svg" style="width:100px; height:auto;">'
-    });
+    const modal = document.getElementById('modal-container');
+    modal.classList.add('show');
+});
+
+document.getElementById("close-modal").addEventListener("click", function() {
+    const modal = document.getElementById('modal-container');
+    modal.classList.remove('show');
 });
 
